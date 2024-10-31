8c6d8a463b1cab055e5aa744826c6f8d9cee1758
maya <maya.n102304@gmail.com>
2024-10-31 14:52:11 -0700

    Added header file, Corrected and added CPP files

---
+++ dynamic array printer/arraySum.h
@@ -0,0 +1,13 @@
+//
+//  arraySum.h
+//  
+//
+//  Created by maya nachiappan on 10/31/24.
+//
+
+#ifndef arraySum_h
+#define arraySum_h
+
+#include <stdio.h>
+void arraySum();
+#endif /* arraySum_h */
