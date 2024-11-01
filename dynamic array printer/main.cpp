f6105c9288aa998be9915b5550ef580a55510bba
maya <maya.n102304@gmail.com>
2024-10-31 21:25:13 -0700

    Update main.cpp

--- dynamic array printer/main.cpp
+++ dynamic array printer/main.cpp
@@ -6,8 +6,33 @@
 //
 
 #include <iostream>
-#include "arraySum.h"
 using namespace std;
+
+void arraySum() // creates a function to find the sum of dynamic array
+{
+    int size;
+    std::cout << "How many numbers do you have:" << std::endl;
+    std::cin >> size; // defines size of the array based on user input
+
+    int* array = new int[size]; // creates a dynamicly allocated array with the user specified size
+
+    std::cout << "Enter " << size << " numbers:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Number " << i + 1 << ": " << std::endl;
+        std::cin >> array[i]; // array is filled with numbers inputted by user
+    }
+
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += array[i]; // adds each element of the array to the sum
+    }
+
+    std::cout << "the sum of the array is " << sum << std::endl;
+
+    // deallocate the array memory
+    delete[] array;
+}
+
 
 int main() 
 {
